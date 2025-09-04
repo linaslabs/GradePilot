825b431cde2b91793d2c65e338af0ec95301a777
Abel Linas <linasabel@gmail.com>
2025-09-04 14:16:03 +0100

    Vercel fix

--- grade-pilot-client/src/App.tsx
+++ grade-pilot-client/src/App.tsx
@@ -11,7 +11,7 @@ import DashboardLayout from './pages/DashboardLayout';
 import Overview from './pages/Overview';
 import YearDetails from './pages/YearDetails';
 import { AuthProvider } from './contexts/AuthContext';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
